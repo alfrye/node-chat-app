c514e104cae59849a12b7ccb64d057812c965891
Alan Frye <alanfrye@Alans-MacBook-Pro.local>
2019-01-06 08:51:54 -0500

    Added  Welcome message and  new user joined message

--- server/server.js
+++ server/server.js
@@ -15,7 +15,19 @@ app.use('/',express.static(publicPath));
 
 io.on('connection', (socket) => {
    console.log('new connection was made');
-  
+    socket.emit('newMessage', {
+       from: 'admin',
+       text: 'Welcome to chat room',
+       createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+      from: 'admin',
+      text: 'New user joined'
+
+    });
+
+
     //socket.emit to a single connections
    //  socket.emit('newMessage', {
    //    from: 'alan',
@@ -26,11 +38,11 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (msg) => {
        console.log('create new message',msg);
        // io.emit will emit messages to all conneceted clients
-       io.emit('newMessage', {
-         from: msg.from,
-         text: msg.text,
-         createdAt: new Date().getTime()
-       });
+      //  io.emit('newMessage', {
+      //    from: msg.from,
+      //    text: msg.text,
+      //    createdAt: new Date().getTime()
+      //  });
     });
    socket.on('disconnect', () => {
     console.log('Server disconnected');
