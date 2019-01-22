9dcb3e11531b64432a9a16ff8b26244daac17c03
Alan Frye <alanfrye@Alans-MacBook-Pro.local>
2019-01-21 21:22:14 -0500

    Added code to make allow for the createLocationMessage to only be sed to speific chat rooms

--- server/server.js
+++ server/server.js
@@ -60,7 +60,9 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude,coords.longitude));
+      var user = users.getUser(socket.id);
+      if(user)
+         io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude,coords.longitude));
     });
    socket.on('disconnect', () => {
     console.log('Server disconnected');
